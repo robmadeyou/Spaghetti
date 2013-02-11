7ae2d9619a2762b15e070918cf690299670ef015
Robert Ellis <robmadeyou@gmail.com>
2013-02-11 14:25:56 +0000

    lines
    lines

--- src/com/gmail/robmadeyou/Wall.java
+++ src/com/gmail/robmadeyou/Wall.java
@@ -2,9 +2,10 @@ package com.gmail.robmadeyou;
 
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glColor4f;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glVertex2i;
+import static org.lwjgl.opengl.GL11.GL_LINES;
 
 public class Wall {
 	
@@ -39,8 +40,8 @@ public class Wall {
 		float r = 1,g = 1,b = 1;
 		if(color.equals("green")){
 			r = 0;
-			g = 0;
-			b = 1;
+			g = 1;
+			b = 0;
 		}else if(color.equals("blue")){
 			r = 0;
 			g = 0;
@@ -59,11 +60,45 @@ public class Wall {
 		//to distinguish them I'll probably add opacity to each and every quad then
 		//so the color is kept the same, just the shade is turned down (as there will be a dark background)
 		glBegin(GL_QUADS);
-			glColor3f(r,g,b);
+			glColor4f(r,g,b,1F);
 			glVertex2i(x, y);
 			glVertex2i(x + w, y);
 			glVertex2i(x + w, y + h);
 			glVertex2i(x, y + h);
+			glColor4f(1,1,1, 1F);
 		glEnd();
+		
+		/* Going to draw a box by just using lines around the wall box, this should give it a FEZ like texture :3
+		 * 1.....2
+		 * .     .
+		 * .     .
+		 * 4.....3
+		 */
+		glColor4f(0,0,0,0.3F);
+		//1 to 2
+		glBegin(GL_LINES);
+			glVertex2i(x, y);
+			glVertex2i(x + w, y);
+		glEnd();
+		
+		//2 to 3
+		glBegin(GL_LINES);
+			glVertex2i(x + w, y);
+			glVertex2i(x + w, y + h);
+		glEnd();
+		
+		//3 to 4
+		glBegin(GL_LINES);
+			glVertex2i(x + w, y + h);
+			glVertex2i(x, y + h);
+		glEnd();
+		
+		//4 to 1
+		glBegin(GL_LINES);
+			glVertex2i(x, y + h);
+			glVertex2i(x, y);
+		glEnd();
+		glColor4f(1,1,1,1F);
+		//Done :3
 	}
 }
